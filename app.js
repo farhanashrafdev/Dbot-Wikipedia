28e53cda73f08e0b1ff20b9c6f4d65e3dd46ade3
Farhan Ashraf <farhanashrafali30@gmail.com>
2021-01-24 20:33:17 +0500

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
 
 client.on('message', msg =>{
   if (msg.content.includes("Who is Pudding?")){
-      msg.reply(["Tauba Tauba sara mood khrab krdia",
+      msg.reply(["Intehai koi Kanjr Admi hai",
       "You can look for him at ",
       "https://www.instagram.com/usman._.h/"
     ])
@@ -17,7 +17,7 @@ client.on('message', msg =>{
 
 client.on('message', msg =>{
     if (msg.content.includes("Who is iceflame?")){
-        msg.reply(["M.A Mohtarma nice choice ","you can look him at",
+        msg.reply(["BHT HE SHAREEF OR MASOOM BACHA HAI ","you can look him at",
         "https://www.instagram.com/mriceflame1/"
       ])
     }
@@ -41,7 +41,7 @@ client.on('message', msg =>{
 client.on('message', msg =>{
     if(msg.author.bot){return}
     if(msg.content === '.commands'){
-        msg.channel.send("I have the following commands : \n .ice \n .mamba \n .aws \n use any :D")
+        msg.channel.send("I have the following commands : \n .ice \n .mamba \n .kuti \n use any :D")
 
     }
     });
@@ -55,8 +55,8 @@ if(msg.content === '.ice'){
 
 client.on('message', msg =>{
     
-    if(msg.content === '.aws'){
-        msg.channel.send("Beta aj rana tay awais nahi bachda. Aj awais nu chuk kay lejana")
+    if(msg.content === '.kuti'){
+        msg.channel.send("RANA SAHAB CHECK KRO", "Aj rana nhi Bachada" )
     }
     });
 
